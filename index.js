372569abe4ec54169c97a2ee0ecded9adb633da1
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-25 10:51:23 +0600

    git igone and vercel json add

--- index.js
+++ index.js
@@ -6,18 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require("./Data/courses.json");
+const coursCategories = require("./Data/coursesCategories.json");
 
 app.get("/", (req, res) => {
   res.send("course start");
+});
+
+app.get("/courses-categories", (req, res) => {
+  res.send(coursCategories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "07") {
+    res.send(courses);
+  } else {
+    const category_courses = courses.filter((e) => e.id === id);
+    res.send(category_courses);
+  }
 });
 
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
-app.get("/course/:id", (req, res) => {
+app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
-  const selectCourse = courses?.find((e) => e.id === id);
+  const selectCourse = courses.find((e) => e.id === id);
   res.send(selectCourse);
 });
 
